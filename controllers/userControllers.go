d60051f261f862944b6b782fa538e44948f8b806
Aditya Mitra <mitraaditya1357@gmail.com>
2023-05-26 03:12:01 +0530

    feat: added login

--- controllers/userControllers.go
+++ controllers/userControllers.go
@@ -2,11 +2,14 @@ package controllers
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"golang-task/initializers"
 	"golang-task/models"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -48,5 +51,68 @@ func SignUp(c *gin.Context) {
 
 
 	// respond
-	c.JSON(http.StatusOK, gin.H{})
+	c.JSON(http.StatusOK, gin.H{ 
+		"message": "User created successfully",
+	})
+}
+
+func Login(c *gin.Context) {
+	// get email/password from request body
+	var body struct {
+		Email string 
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Fields are empty",
+		})
+		return	
+	}
+
+	// find user by email
+	var user models.User
+	initializers.DB.First(&user, "email = ?", body.Email)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "User not found",
+		})
+		return
+	}
+
+
+	// compare password
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid password",
+		})
+		return
+	}
+
+	// generate jwt
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),	
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Error generating jwt",
+		})
+		return
+	}
+
+	// respond with jwt
+	c.SetSameSite(http.SameSiteNoneMode)
+	c.SetCookie("jwt", tokenString, 3600*24*30, "", "", false, true)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Logged in successfully",
+	})
 }
--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func init() {
 func main() {
 	r := gin.Default()
 	r.POST("/signup", controllers.SignUp)
-
+	r.POST("/login", controllers.Login)
+	
 	r.Run() 
 }
