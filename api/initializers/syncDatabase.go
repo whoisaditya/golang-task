f8e6068f9e4466b4e1b2e01e2d6dd230c9620145
Aditya Mitra <mitraaditya1357@gmail.com>
2023-07-25 00:00:08 +0530

    fix: file path

--- api/initializers/syncDatabase.go
+++ api/initializers/syncDatabase.go
@@ -1,6 +1,6 @@
 package initializers
 
-import "github.com/whoisaditya/golang-task/models"
+import "github.com/whoisaditya/golang-task/api/models"
 
 func SyncDatabase() {
 	DB.AutoMigrate(&models.User{})
